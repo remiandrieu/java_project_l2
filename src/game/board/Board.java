3a1116e55455b7922457f81742b4c08d72ecbd6f
remiandrieu <remi.andrieu@outlook.fr>
2025-01-15 09:16:18 +0100

    getNeighbourTiles in Board

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -19,7 +19,7 @@ public class Board {
     }
 
     /**
-     * Get the tile at line x and at column y
+     * Get the tile at line x and column y
      * @param x the line number
      * @param y the column number
      * @return The tile at position (x, y)
@@ -29,7 +29,26 @@ public class Board {
     }
 
     /**
-     * Set the tile at line x and at column y
+     * Get the neighbours of the tile at line x and column y
+     * @param x the line number
+     * @param y the column number
+     * @return The neighbours of the tile at position (x, y)
+     */
+    public Tile[] getNeighbourTiles(int x, int y){
+        Tile[] res = new Tile[4];
+        if (x-1 > 0)
+            res[0] = this.getTile(x-1, y);
+        if (y+1 < this.WIDTH)
+            res[1] = this.getTile(x, y+1);
+        if (x+1 < this.LENGTH)
+            res[2] = this.getTile(x+1, y);
+        if (y-1 > 0)
+            res[3] = this.getTile(x, y-1);
+        return res;
+    }
+
+    /**
+     * Set the tile at line x and column y
      * @param x the line number
      * @param y the column number
      * @param tile the tile
