e0d0b6a1d0de232783a654193814317015da23c6
remiandrieu <remi.andrieu@outlook.fr>
2025-01-15 08:58:04 +0100

    getTile in Board

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -8,7 +8,7 @@ public class Board {
     protected final int LENGTH;
     protected final int WIDTH;
 
-    /*
+    /**
      * Create a board with a length and a width
      * @param length the length of the board
      * @param width the width of the board
@@ -16,5 +16,15 @@ public class Board {
     public Board(int length, int width){
         this.LENGTH = length;
         this.WIDTH = width;
+    }
+
+    /**
+     * Get the tile at line x and at column y
+     * @param x the line number
+     * @param y the column number
+     * @return The tile at position (x, y)
+     */
+    public Tile getTile(int x, int y){
+        return this.grid[x][y];
     }
 }
