f7f0def1fed6864eddb233b104557d45285d5cb9
remiandrieu <remi.andrieu@outlook.fr>
2025-01-29 09:26:27 +0100

    Livrable1.java done

--- src/game/board/Livrable1.java
+++ src/game/board/Livrable1.java
@@ -2,8 +2,29 @@ package game.board;
 
 public class Livrable1 {
     public static void main(String[] args) throws InvalidPositionException {
-        Board board = new Board(7, 6);
-        board.createGrid();
-        System.out.println(board.boardToString(false));
+        int length;
+        int width;
+        boolean error = false;
+        
+        if (args.length >= 2){
+            try{
+                length = Integer.parseInt(args[0]);
+                width = Integer.parseInt(args[1]);
+                Board board = new Board(length, width);
+                board.createGrid();
+                System.out.println(board.boardToString(false));
+            } catch (Exception e){
+                error = true;
+            }
+        } else {
+            error = true;
+        }
+
+        if (error) {
+            System.out.println("How to use :");
+            System.out.println("livrable1.java <length> <width>");
+            System.out.println("<length> : length of the board >= 3");
+            System.out.println("<width> : width of the board >= 3");
+        }
     }
 }
