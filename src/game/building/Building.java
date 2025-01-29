0ee2b86461af4d02e61407c752dfb4a856b10607
Charonov Andrei <andrei.charonov.etu@a14p14.fil.univ-lille.fr>
2025-01-29 09:31:06 +0100

    Week4: constructeur et attributs building

--- src/game/building/Building.java
+++ src/game/building/Building.java
@@ -1,5 +1,13 @@
 package game.building;
 
+import game.board.*;
+
 public abstract class Building {
-    
+    protected Player player;
+    protected Land land;
+
+    public Building(Player player, Land land) {
+        this.player = player;
+        this.land = land;
+    }
 }
