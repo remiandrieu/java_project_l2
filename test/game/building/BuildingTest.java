419dd76abc13091cd72255a8710625306c2dbb3b
Charonov Andrei <andrei.charonov.etu@a15p15.fil.univ-lille.fr>
2025-02-03 08:19:04 +0100

    Week 5: Création Classe test Building

---
+++ test/game/building/BuildingTest.java
@@ -0,0 +1,12 @@
+package game.building;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import game.board.*;
+import game.player.*;
+
+public class BuildingTest {
+
+}
