b79f603c0c8f90f54995b1fcb46e46f82a25b3f0
Demon Samuel <samuel.demon.etu@a15p14.fil.univ-lille.fr>
2025-02-03 08:24:49 +0100

    added Port.java

---
+++ src/game/building/Port.java
@@ -0,0 +1,15 @@
+package game.building;
+import game.board.*;
+import game.player.*;
+
+/** a class to represent the ports */
+public class Port extends Building {
+
+    /**
+     * creates a port
+     */
+    public Port(Player player, Land land){
+        super(player, land);
+    }
+
+}
