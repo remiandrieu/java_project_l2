555ad571ba32cf726bf072dd9a5af08ae32a4822
Samuel Demon <samuel.demon.etu@univ-lille.fr>
2025-02-20 11:25:54 +0100

    Livrable2.java modifié pour entrer les arguments

--- src/game/building/Livrable2.java
+++ src/game/building/Livrable2.java
@@ -4,11 +4,14 @@ import game.board.*;
 
 public class Livrable2 {
     public static void main(String[] args) throws InvalidPositionException {
-        Board board = new Board(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        int length = 10;
+        int width = 10;
+        if (args.length > 2){
+            length = Integer.parseInt(args[0]);
+            width = Integer.parseInt(args[1]);
+        }
+        Board board = new Board(length, width);
         board.createGrid();
-        System.out.println(board.boardToString(false));
-        System.out.println(board);
-        
-
+        System.out.println(board.boardToString(true));
     }
 }
