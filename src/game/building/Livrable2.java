46990cf2e32fd8b6bcc66d8b99da78bd1c60c45a
Samuel Demon <samuel.demon.etu@univ-lille.fr>
2025-02-21 15:03:53 +0100

    adding the possibility to choose your board size for Livrable2

--- src/game/building/Livrable2.java
+++ src/game/building/Livrable2.java
@@ -7,7 +7,7 @@ public class Livrable2 {
     public static void main(String[] args) throws InvalidPositionException {
         int length = 10;
         int width = 10;
-        if (args.length > 2){
+        if (args.length >= 2){
             length = Integer.parseInt(args[0]);
             width = Integer.parseInt(args[1]);
         }
