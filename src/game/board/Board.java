53ce9fca8195f9b5eb4dee4bab26b268d6433da7
Taffin Tom <taffin.tom59@gmail.com>
2025-02-21 15:57:12 +0100

    resolution probleme

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -253,8 +253,6 @@ public class Board {
                 Tile tile = this.getTile(x, y);
                 if (tile instanceof Sea){
                     res += ".  ";
-                } else if (buildings && tile.hasBuilding()){
-                    res += tile.toString().charAt(0) + tile.building.toString().charAt(0) + tile.building.player.getId();
                 } else {
                     res += tile.toString().charAt(0) + "  ";
                 }      
