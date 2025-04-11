b59e0b2f2179d05a1a143e6ba8adbdc7e8835d4a
Taffin Tom <taffin.tom59@gmail.com>
2025-04-11 15:03:42 +0200

    aresConquerTiles

---
+++ src/game/objective/AresConquerTiles.java
@@ -0,0 +1,29 @@
+package game.objective;
+
+import game.board.Board;
+import game.player.Player;
+
+/* a class to model the conquer nb tiles objective for Ares Game */
+public class AresConquerTiles extends AresObjective{
+    
+    /* variables */
+    /*TODO expression for nbTile */
+    public static final int nbTiles = 8;
+
+    /**
+     * Create the ConquerIsland objective
+     * @param player the player which must achieve this objective
+     * @param board the board
+     */
+    public AresConquerTiles(Player player, Board board){
+        super(player, board, "Conquer "+nbTiles+" tiles");
+    }
+    
+    /**
+     * Checks if the player conquered nbTiles tiles.
+     * @return true if the player conquered an nbTiles tiles.
+     */
+    public boolean isAchieved(){
+        return this.player.getBuildings().size()>=nbTiles;
+    }
+}
